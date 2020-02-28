8dc1f6d2c340c01269e3aa1afef9303693a1a1bb
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-02-27 19:05:23 -0500

    Update main.js

--- 2. Clock/main.js
+++ 2. Clock/main.js
@@ -1,4 +1,5 @@
 
+// Instatiate clock hands from the DOM
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
